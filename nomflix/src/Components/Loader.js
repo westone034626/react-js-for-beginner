86b4c4a57eb9ddfca33df94566df2f4c51e0d36c
westone034626 <dydrlaks@likelion.org>
2020-12-13 17:02:56 +0900

    feat: Progress course until chap6.2

---
+++ nomflix/src/Components/Loader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  font-size: 32px;
+`;
+
+export default () => (
+  <Container>
+    <span role="img" aria-label="Loading">
+      ⏰
+    </span>
+  </Container>
+);
